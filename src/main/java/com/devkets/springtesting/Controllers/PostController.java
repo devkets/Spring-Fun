102d2efbbcc16dfb1fb51af100650396a73a8535
Andrew Lewis <devkets@gmail.com>
2023-05-30 08:23:19 -0500

    Additional Sudoku

--- src/main/java/com/devkets/springtesting/Controllers/PostController.java
+++ src/main/java/com/devkets/springtesting/Controllers/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devkets.springtesting.Models.PostRequestModel;
 import com.devkets.springtesting.Models.PostResponseModel;
+import com.devkets.springtesting.Models.SudokuRequestModel;
 import com.devkets.springtesting.Services.PostService;
 
 @RestController
@@ -22,6 +23,15 @@ public class PostController {
         PostResponseModel response = new PostResponseModel();
         response.setAccountNumber(request.getAccountNumber());
         response.setMessage("Successful passthrough");
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping(value="/verifySudoku")
+    public ResponseEntity<PostResponseModel> verifySudoku(@RequestBody SudokuRequestModel request) {
+
+        PostResponseModel response = new PostResponseModel();
+        response = postService.validateSudokuMatrix(request);
 
         return ResponseEntity.ok().body(response);
     }
---
+++ src/main/java/com/devkets/springtesting/Models/SudokuRequestModel.java
@@ -0,0 +1,17 @@
+package com.devkets.springtesting.Models;
+
+import java.util.List;
+
+public class SudokuRequestModel {
+    
+    private List<String> lines;
+
+    public List<String> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<String> lines) {
+        this.lines = lines;
+    }
+
+}
